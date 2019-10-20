df96754329d930e594a47d5e9cf3506fd24cefc3
khalidalhussaini <55993175+khalidalhussaini@users.noreply.github.com>
2019-10-20 14:49:13 +0200

    Add files via upload

---
+++ bonnus/ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktabit <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/11 19:45:43 by ktabit            #+#    #+#             */
+/*   Updated: 2019/10/11 19:52:11 by ktabit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
+{
+	if (alst != NULL && del != NULL)
+	{
+		(*del)((*alst)->content, (*alst)->content_size);
+		ft_memdel((void **)alst);
+	}
+}
